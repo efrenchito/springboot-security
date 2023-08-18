8fcef84ed7089d75e6f98b5a2d7a84f3693caf3d
efrenchito <efrenchito@gmail.com>
2023-08-18 08:55:15 -0500

    Multiple Users (UserDetailService)

--- src/main/java/com/learningspring/springbootsecurity/security/WebSecurity.java
+++ src/main/java/com/learningspring/springbootsecurity/security/WebSecurity.java
@@ -6,8 +6,12 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +29,24 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                .antMatchers(HttpMethod.GET).permitAll()
                .anyRequest().authenticated()
             .and().httpBasic();
+
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        //UserDetails user = User.builder().username("ediaz").password("ediaz").roles("USER").build();
+        //UserDetails admin=User.builder().username("admin").password("admin").roles("ADMIN").build();
+
+        UserDetails user = User.builder()
+                .username("ediaz")
+                .password(passwordEncoder().encode("ediaz"))
+                .roles("USER").build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user, admin);
 
     }
 }
