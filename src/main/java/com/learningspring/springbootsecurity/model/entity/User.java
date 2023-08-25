4b6d34b76aa5df8f3e2a897f648dc5b4b53df46c
efrenchito <efrenchito@gmail.com>
2023-08-25 07:29:33 -0500

    Add ManyToMany relationship

--- src/main/java/com/learningspring/springbootsecurity/model/entity/User.java
+++ src/main/java/com/learningspring/springbootsecurity/model/entity/User.java
@@ -3,6 +3,7 @@ package com.learningspring.springbootsecurity.model.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -25,4 +26,12 @@ public class User {
     private String username;
     private String password;
     private String email;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Role> roles;
 }
