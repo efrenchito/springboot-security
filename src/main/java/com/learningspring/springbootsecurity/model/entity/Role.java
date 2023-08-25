076a0b22a15f9df0bfe18d668118090ea91b1882
efrenchito <efrenchito@gmail.com>
2023-08-25 07:24:31 -0500

    Add Entity annotations to User and Role classes

--- src/main/java/com/learningspring/springbootsecurity/model/entity/Role.java
+++ src/main/java/com/learningspring/springbootsecurity/model/entity/Role.java
@@ -2,12 +2,19 @@ package com.learningspring.springbootsecurity.model.entity;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
+@Table(name = "roles")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(length = 60)
     private String name;
 }
--- src/main/java/com/learningspring/springbootsecurity/model/entity/User.java
+++ src/main/java/com/learningspring/springbootsecurity/model/entity/User.java
@@ -2,12 +2,24 @@ package com.learningspring.springbootsecurity.model.entity;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
+@Table(
+        name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"username"}),
+                @UniqueConstraint(columnNames = {"email"})
+        }
+)
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String username;
