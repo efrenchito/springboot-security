5b484c166caa9e68516304c91f4a5237a568ac1d
efrenchito <efrenchito@gmail.com>
2023-09-08 11:01:46 -0500

    Authentication Controller - Sign up

---
+++ src/main/java/com/learningspring/springbootsecurity/controller/AuthenticationController.java
@@ -0,0 +1,25 @@
+package com.learningspring.springbootsecurity.controller;
+
+import com.learningspring.springbootsecurity.model.dto.LoginDto;
+import com.learningspring.springbootsecurity.model.dto.SignupDto;
+import com.learningspring.springbootsecurity.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody SignupDto signupDto) {
+        return authenticationService.signup(signupDto);
+    }
+
+}
