0dfcf7c74cde8d67f6f1303304942e658af10cf1
efrenchito <efrenchito@gmail.com>
2023-09-08 11:02:51 -0500

    AuthenticationManager Bean

--- src/main/java/com/learningspring/springbootsecurity/security/WebSecurity.java
+++ src/main/java/com/learningspring/springbootsecurity/security/WebSecurity.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -24,6 +25,12 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     private final CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
